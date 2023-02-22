50c79a8ec08c9f7421b18601bff6965d2ac25d01
Everton <54149497+EvertonWingert@users.noreply.github.com>
2023-02-22 09:42:50 -0300

    Update server.ts

--- server.ts
+++ server.ts
@@ -2,8 +2,8 @@ import express from "express";
 
 const app = express();
 const router = express.Router();
+app.use(express.json());
 
-// respond with "hello world" when a GET request is made to the homepage
 router.get("/", function (req, res) {
   res.json({
     message: "Hello World!",
@@ -11,8 +11,6 @@ router.get("/", function (req, res) {
 });
 
 
-app.use(express.json());
 app.use(router);
-
 
 export default app;
